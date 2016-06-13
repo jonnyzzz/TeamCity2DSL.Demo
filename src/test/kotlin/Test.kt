856fa7db2a9daa0e3d78218ae35450f14d104d13
Eugene Petrenko <eugene.petrenko@gmail.com>
2016-06-13 17:20:18 +0200

    test support

---
+++ src/test/kotlin/Test.kt
@@ -0,0 +1,13 @@
+import org.jonnyzzz.teamcity.dsl_generated.Build_Jonnyzzz_Test
+import org.junit.Test
+
+/**
+ * Created by eugene.petrenko@gmail.com
+ */
+
+class DSLTest {
+  @Test
+  fun testSomeDSLProperty() {
+    println(Build_Jonnyzzz_Test.id)
+  }
+}
